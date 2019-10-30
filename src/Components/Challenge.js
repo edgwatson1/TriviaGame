5eea64c003d651e323662019f3844cfa6bdb3a28
edgwatson1 <e@edwardwatson.com>
2019-10-30 18:01:00 +0000

    done

--- src/Components/Challenge.js
+++ src/Components/Challenge.js
@@ -6,57 +6,26 @@ class Challenge extends React.Component {
     super(props);
   }
 
-  render = () => {
-    const questionsPackages = this.props.questionsPackages;
-    const questionPackage1 =
-      questionsPackages[Math.floor(Math.random() * Math.floor(51))];
-    // const questionPackage2 =
-    //   questionsPackages[Math.floor(Math.random() * Math.floor(51))];
-    // const questionPackage3 =
-    //   questionsPackages[Math.floor(Math.random() * Math.floor(51))];
-    // const questionPackage4 =
-    //   questionsPackages[Math.floor(Math.random() * Math.floor(51))];
-    // const questionPackage5 =
-    //   questionsPackages[Math.floor(Math.random() * Math.floor(51))];
-    // const questionPackage6 =
-    //   questionsPackages[Math.floor(Math.random() * Math.floor(51))];
-    // const questionPackage7 =
-    //   questionsPackages[Math.floor(Math.random() * Math.floor(51))];
-    // const questionPackage8 =
-    //   questionsPackages[Math.floor(Math.random() * Math.floor(51))];
-    // const questionPackage9 =
-    //   questionsPackages[Math.floor(Math.random() * Math.floor(51))];
-    // const questionPackage10 =
-    //   questionsPackages[Math.floor(Math.random() * Math.floor(51))];
 
-    const correctAnswerPosition1 = Math.floor(Math.random() * Math.floor(4));
-    // const correctAnswerPosition2 = Math.floor(Math.random() * Math.floor(4));
-    // const correctAnswerPosition3 = Math.floor(Math.random() * Math.floor(4));
-    // const correctAnswerPosition4 = Math.floor(Math.random() * Math.floor(4));
-    // const correctAnswerPosition5 = Math.floor(Math.random() * Math.floor(4));
-    // const correctAnswerPosition6 = Math.floor(Math.random() * Math.floor(4));
-    // const correctAnswerPosition7 = Math.floor(Math.random() * Math.floor(4));
-    // const correctAnswerPosition8 = Math.floor(Math.random() * Math.floor(4));
-    // const correctAnswerPosition9 = Math.floor(Math.random() * Math.floor(4));
-    // const correctAnswerPosition10 = Math.floor(Math.random() * Math.floor(4));
 
-    const allFourAnswers1 = questionPackage1.incorrect_answers;
-    const splicer1 = allFourAnswers1.splice(
-      correctAnswerPosition1,
-      0,
-      questionPackage1.correct_answer
-    );
+  render() {
+    // const questionPackage1 = questionsPackages[Math.floor(Math.random() * Math.floor(51))]; 
+
+    // const correctAnswerPosition1 = Math.floor(Math.random() * Math.floor(4));
+    
+    // const allFourAnswers1 = questionPackage1.incorrect_answers;
+    
+    // const splicer1 = allFourAnswers1.splice(correctAnswerPosition1, 0, questionPackage1.correct_answer);
 
     return (
       <>
         <p>This is the challenge container</p>
-        <p>{this.props.Information}</p>
         <div className="ui cards">
           <div className="card">
             <div className="content"></div>
-            <QandA questionPackage={questionPackage1} />
+            <QandA questionPackages={this.props.questionPackages} />
             <br />
-            <button style={{ color: "red" }}>Next Question</button>
+            <button>Next Question</button>
             <br />
           </div>
         </div>
--- src/Components/QandA.js
+++ src/Components/QandA.js
@@ -3,15 +3,41 @@ import QuestionBox from "./QuestionBox";
 import AnswerButs from "./AnswerButs";
 
 const QandA = props => {
-  const questionPackage = props.questionPackage;
+  // const questionPackage = props.questionPackage;
 
-  const correctAnswerPosition1 = Math.floor(Math.random() * Math.floor(4));
-  const answersArray = questionPackage.incorrect_answers;
-  const emptyMixer = answersArray.splice(
-    correctAnswerPosition1,
-    0,
-    questionPackage.correct_answer
-  );
+  // const correctAnswerPosition1 = Math.floor(Math.random() * Math.floor(4));
+  // const answersArray = questionPackage.incorrect_answers;
+  // const emptyMixer = answersArray.splice(
+  //   correctAnswerPosition1,
+  //   0, //   questionPackage.correct_answer
+  // );
+  
+;
+
+// selecting one question from the question package in position [0]
+const oneQuestion = props.questionPackages[0].question;
+console.log(oneQuestion);
+
+// generating one random number from 0, 1, 2, 3
+const randomPosition = Math.floor(Math.random() * Math.floor(4));
+console.log(randomPosition);
+
+In &quot;Fairy Tail&quot;, what is the nickname of Natsu Dragneel?
+// correct answer from the question package in position [0]
+const correctAnswer = props.questionPackages[0].correct_answer;
+console.log(correctAnswer);
+
+
+// takes the incorrect answers from the question package in position [0]
+const answersArray = props.questionPackages[0].incorrect_answers
+console.log(answersArray);
+
+// manipulating answersArray to splice in the correct answer at a random position
+const emptyMixer = answersArray.splice(randomPosition,0,correctAnswer);
+console.log(answersArray);
+
+
+
 
   return (
     <div>
@@ -21,8 +47,8 @@ const QandA = props => {
           <div className="content">
             <div className="meta">...</div>
             <div className="question">
-              <QuestionBox questionStr={questionPackage.question} />
-              <AnswerButs mixedAnswers={answersArray} />
+              <QuestionBox questionStr={props.questionPackages.question} />
+              {/* <AnswerButs mixedAnswers={answersArray} /> */}
             </div>
           </div>
         </div>
