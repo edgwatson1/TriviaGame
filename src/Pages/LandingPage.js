a313ba565ae3061b3f5932bf7cd0e6156d46ba29
Edward Watson <e@edwardwatson.com>
2019-11-14 16:15:59 +0000

    update

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -10,34 +10,46 @@ function LandingPage() {
         </h1>
         {/* landing page theme song */}
         <audio src="/landing.mp3" autoPlay loop hidden></audio>
-      </div>
-
-      <div class="footer">
-        <Link to="CategoryWheel">
-          <button class="btn">
-            <a href="#">Play Now!</a>
-          </button>
-        </Link>
-      </div>
-
-      <div class="madebyfooter">
-        <div class="david">
-          <img src="avatardavid.png" height="80px"></img> <br></br>David
-          <br></br>Saiote
+        <div class="footer">
+          <Link to="CategoryWheel">
+            <button class="btn" id="playnow">
+              <a href="#">Play!</a>
+            </button>
+          </Link>
         </div>
-        <div class="alex">
-          <img src="avataralex.png" height="80px"></img> <br />
-          Alex <br />
-          Lawry-White
+        <div class="madebyfooterwrapper">
+          <div class="madebyfooter">
+            <div class="david">
+              <img src="avatardavid.png" height="80px"></img>
+              <p id="name">
+                <br></br>David
+                <br></br>Saiote
+              </p>
+            </div>
+            <div class="alex">
+              <img src="avataralex.png" height="80px"></img>
+              <p id="name">
+                {" "}
+                <br />
+                Alex <br />
+                Lawry-White
+              </p>
+            </div>
+            <div class="edward">
+              <img src="avataredward.png" height="80px"></img>{" "}
+              <p id="name">
+                <br />
+                Edward
+                <br />
+                Watson
+                <br /> <br />
+              </p>
+            </div>
+            <div class="madebymessage">
+              Made with <img src="8bit-heart.png" height="25px"></img> by:
+            </div>
+          </div>
         </div>
-        <div class="edward">
-          <img src="avataredward.png" height="80px"></img> <br />
-          Edward
-          <br />
-          Watson
-          <br /> <br />
-        </div>
-        <div class="madebymessage">Made with love by:</div>
       </div>
     </>
   );
